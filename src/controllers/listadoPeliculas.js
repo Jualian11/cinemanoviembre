ba50fdc41e735b7bf70b5be3997af2b14d165d0f
BerMira <bamg359@gmail.com>
2022-11-21 16:46:34 -0500

    se configura memoria

--- src/controllers/listadoPeliculas.js
+++ src/controllers/listadoPeliculas.js
@@ -2,53 +2,64 @@ let peliculas=[
     {
         nombre:"Hallowen la noche final",
         duracion:120,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/hallowen%20ultimo%20poster.jpg?alt=media&token=4cc9a9c8-c7bb-4462-9675-2dd057e889f0"
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/hallowen%20ultimo%20poster.jpg?alt=media&token=4cc9a9c8-c7bb-4462-9675-2dd057e889f0",
+        sinopsis:"Un psiquiatra sigue el camino sangriento de un psicópata que se ha escapado de una institución mental luego de estar internado 17 años y que huye a su pueblo natal."
 
     },
     {
         nombre:"La noche del demonio",
         duracion:90,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/jeeppers.jpg?alt=media&token=c6c07002-c476-4095-900f-011047e967dc"
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/jeeppers.jpg?alt=media&token=c6c07002-c476-4095-900f-011047e967dc",
+        sinopsis:"osh (Patrick Wilson), su esposa Renai (Rose Byrne) y sus tres hijos acaban de mudarse a una vieja casa. Pero, tras un desgraciado accidente, uno de los niños entra en coma y, al mismo tiempo, empiezan a producirse en la casa extraños fenómenos que aterrorizan a la familia."
     },
     {
         nombre:"Minions",
         duracion:85,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/minions2posternuevo.jpg?alt=media&token=dc7c97ca-ebb7-4342-b0c9-a9fd7b0f1af1"
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/minions2posternuevo.jpg?alt=media&token=dc7c97ca-ebb7-4342-b0c9-a9fd7b0f1af1",
+        sinopsis:"lorem"
     },
     {
         nombre:"Tadeo",
         duracion:104,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/tadeo3nuevoposter.jpg?alt=media&token=3b31d4c0-3c0b-4317-ba8a-fc47a47e2a84"
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/tadeo3nuevoposter.jpg?alt=media&token=3b31d4c0-3c0b-4317-ba8a-fc47a47e2a84",
+        sinopsis:"lorem"
     },
     {
         nombre: "Wakanda forever",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690"
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690",
+        sinopsis:"lorem"
+
     },
     {
         nombre: "Wakanda forever",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690"
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690",
+        sinopsis:"lorem"
     },
     {
         nombre: "Wakanda forever",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690"
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690",
+        sinopsis:"lorem"
     },
     {
         nombre: "Wakanda forever",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690"
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690",
+        sinopsis:"lorem"
     },
     {
         nombre: "Wakanda forever",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690"
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690",
+        sinopsis:"lorem"
     },
     {
         nombre: "Wakanda forever",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690"
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690",
+        sinopsis:"lorem"
     }
 ]
 
@@ -81,13 +92,40 @@ peliculas.forEach(function(pelicula){
     duracion.classList.add("text-start")
     duracion.textContent="Duración: "+pelicula.duracion+" Min"
 
+    let sinopsis=document.createElement("p")
+    sinopsis.classList.add("d-none")
+    sinopsis.textContent=pelicula.sinopsis
+
+
+
     //PADRES E HIJOS
     etiquetaFila.appendChild(columna)
     columna.appendChild(tarjeta)
     tarjeta.appendChild(poster)
     tarjeta.appendChild(nombre)
     tarjeta.appendChild(duracion)
+    tarjeta.appendChild(sinopsis)
 
     
 
+})
+
+
+//Escuchando el evento clic en alguna pelicula de la cartelera:
+let peliculaSeleccionada={}
+etiquetaFila.addEventListener("click",function(evento){
+    if(evento.target.classList.contains("img-fluid")){
+       
+        //Obteniendo los datos de la pelicula
+        peliculaSeleccionada.imagen=evento.target.parentElement.querySelector('img').src
+        peliculaSeleccionada.titulo=evento.target.parentElement.querySelector('h3').textContent
+        peliculaSeleccionada.duracion=evento.target.parentElement.querySelector('h5').textContent
+        peliculaSeleccionada.sinopsis=evento.target.parentElement.querySelector('p').textContent
+
+       //enviando nuestros datos de la pelicula seleccionada a la memoria
+       localStorage.setItem("pelicula",JSON.stringify(peliculaSeleccionada))
+
+       window.location.href="./src/views/verinfopelicula.html"
+
+    }
 })
